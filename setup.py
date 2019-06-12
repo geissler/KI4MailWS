0e38794e1c92dafbf240cd9aec6e628d9aaad76c
Benjamin <benjamin.geissler@gmail.com>
2019-06-12 23:33:51 +0200

    setup changed

--- setup.py
+++ setup.py
@@ -1,1 +1,42 @@
-print("Do some pip setup?")
+import os
+import shutil
+
+copy_destinations = [{
+    'src': 'conf/web.ini',
+    'target': '../conf/',
+    'file': 'web.ini',
+    'overwrite': False
+}, {
+    'src': 'src/KI4MailWS/wsdl.py',
+    'target': '../src/KI4MailWS/',
+    'file': 'wsdl.py',
+    'overwrite': True
+}, {
+    'src': 'web.py',
+    'target': '../',
+    'file': 'web.py',
+    'overwrite': True
+}]
+
+file_name = os.path.basename(__file__)
+folder = os.path.realpath(__file__).replace(file_name, '')
+
+for copy in copy_destinations:
+    if os.path.isfile(folder + copy['target'] + copy['file']):
+        if copy['overwrite']:
+            os.remove(folder + copy['target'] + copy['file'])
+        else:
+            next()
+
+    required_folders = os.path.realpath(os.path.join(folder, copy['target'])).split('/')
+    full_path = '/'
+    for path_folder in required_folders:
+        if path_folder != "":
+            full_path = full_path + "/" + path_folder
+        if not os.path.isdir(full_path):
+            os.mkdir(full_path)
+
+    os.rename(folder + copy['src'], os.path.realpath(os.path.join(folder, copy['target'])) + "/" + copy['file'])
+
+# remove self
+shutil.rmtree(folder)
